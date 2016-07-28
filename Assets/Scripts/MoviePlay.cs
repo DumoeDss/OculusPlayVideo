0e478510ada9f6bcd26871d603a6269174ce5f8e
DumoeDss <ws11579@gmail.com>
2016-07-28 11:52:10 +0800

    update

--- Assets/Scripts/MoviePlay.cs
+++ Assets/Scripts/MoviePlay.cs
@@ -5,18 +5,16 @@ public class MoviePlay : MonoBehaviour {
     public MovieTexture movieTexture;
 	void Start ()
     {
+        //设置贴图
         GetComponent<Renderer>().material.mainTexture = movieTexture;
         //设置电影纹理播放模式为循环    
-        movieTexture.loop = true;  
-        AudioSource MainCameraAudioSource = Camera.main.GetComponent(typeof(AudioSource)) as AudioSource;
+        movieTexture.loop = true;
+        //添加AudioSource
+        AudioSource mainCameraAudioSource = Camera.main.GetComponent(typeof(AudioSource)) as AudioSource;
         //获取视频的音频文件并为AudioSource的clip属性赋值  
-        MainCameraAudioSource.clip = movieTexture.audioClip; 
+        mainCameraAudioSource.clip = movieTexture.audioClip; 
         //开始播放  
         movieTexture.Play();
-        MainCameraAudioSource.Play();  
-	}
-	
-	void Update () {
-	
+        mainCameraAudioSource.Play();  
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OculusPlayVideo
 The Oculus app that simulate the effect of the cinema.
+
+该程序基于Oculus的Unity SDK，模拟电影院巨屏的效果。
+
+使用`Assets/Prefabs`文件夹下的**Curved screen**预设体作为屏幕，利用MovieTexture播放视频。
+
+调整 **Curved screen**的z轴的缩放，可以改变屏幕弯曲程度。
